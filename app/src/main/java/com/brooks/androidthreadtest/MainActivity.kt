f82c080f1f26a1dff62b41981036d1ed205b283f
brooks <myt2000@126.com>
2022-11-21 17:03:51 +0800

    实现子线程刷新UI

--- app/src/main/java/com/brooks/androidthreadtest/MainActivity.kt
+++ app/src/main/java/com/brooks/androidthreadtest/MainActivity.kt
@@ -2,10 +2,38 @@ package com.brooks.androidthreadtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
+    val updateText = 1
+
+    val handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            // 在这里可以进行UI操作
+            Log.d("Message消息", "handleMessage: ${msg}")
+            super.handleMessage(msg)
+            when (msg.what) {
+                updateText -> textView.text = "Nice to meet you"
+            }
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        changeTextBtn.setOnClickListener {
+            thread {
+                val msg = Message()
+                msg.what = updateText
+//                textView.text = "Nice to meet you"
+                handler.sendMessage(msg)    // 将Message对象发送出去
+            }
+        }
     }
 }
